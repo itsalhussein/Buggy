7ecd1ebb10fb1e79225334de0880e867b0953553
Hussein <hussein.elsherief@gmail.com>
2022-04-12 02:40:09 +0200

    - Added some code and debugged all the errors caused the application to crash

--- Buggy/ViewController.swift
+++ Buggy/ViewController.swift
@@ -13,7 +13,30 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    func badMethod() {
+        let array = NSMutableArray()
+        
+        for i in 0..<10 {
+            array.insert(i, at: i)
+        }
+        
+        for _ in 0..<10 {
+            array.removeObject(at: 0)
+        }
+    }
 
+    
+
+    
+    
+    //MARK: - Actions
+    
+    @IBAction func buttonTapped(_ sender: UIButton){
+        print("Method: \(#function) , in file: \(#file) , line \(#line) called")
+        badMethod()
+    }
 
 }
 
